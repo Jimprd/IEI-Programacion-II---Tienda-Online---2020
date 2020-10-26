15f795a171a88f65d9bf1953ad79b7b9fa1ac647
Joel <joelimparado@gmail.com>
2020-10-26 19:01:25 -0300

    Refactorización del código fuente en app.js, más legible y se añade routes de pedidos

--- api/app.js
+++ api/app.js
@@ -1,37 +1,65 @@
-const [express, bodyParser, morgan, cors] = [require('express'), require('body-parser'), require('morgan'), require('cors')]; 
+const [express, bodyParser, morgan, cors, config] = [
+  require("express"),
+  require("body-parser"),
+  require("morgan"),
+  require("cors"),
+  require("./config/configuracion"),
+];
+/**
+ * BASE DE DATOS
+ * Al fichero de base de datos solo necesitamos requerirlo para que esté disponible en la aplicación
+ * No hace falta asignarla a una variable, ya que no utilizaremos ningún método o función desde app.js.
+ */
+require("./databases/mongodb");
 
-// require('./databases/mongodb');
-const mongodb = require('./databases/mongodb'); // carga el módulo y parece que mágicamente se conecta a la DB en tiempo de ejecucion. Si no hago esto no se conecta, en el tutorial hacía un metodo (VERRRRR).
+const [rtsProducto, rtsUsuario, rtsCliente, rtsRoot, rtsPedido] = [
+  require("./routes/producto"),
+  require("./routes/usuario"),
+  require("./routes/cliente"),
+  require("./routes/saludar"),
+  require("./routes/pedidos"),
+  // const routes = require('./routes/routes')
+];
 
-const rtsProducto = require('./routes/producto')
-    , rtsUsuario = require('./routes/usuario')
-    , rtsCliente = require('./routes/cliente')
-    // , rtsPedido = require('./routes/pedido');
-// const routes = require('./routes/routes');
-
-
-// Instantiate a new Express application.
+// Instantiate a new Express application
 const app = express();
 
+/**
+ * MIDDLEWARES
+ * morgan mide tiempos de respuestas get post, etc y realiza logs en terminal
+ * bodyParser.json() es el encargado de que una petición POST coloque los parámetros en req.params o nos facilite los archivos de haberlos.
+ */
+app.use([
+  morgan("dev"),
+  bodyParser.json(),
+  bodyParser.urlencoded({ extended: false }),
+  cors(),
+]);
 
-//MIDDLEWARES
-app.use(morgan('dev')); // tiempos de respuestas get post, etc
-app.use(bodyParser.json()); // body-parser es el encargado de que una petición POST coloque los parámetros en req.params o nos facilite los archivos de haberlos.
-app.use(bodyParser.urlencoded({extended: false}));// 
+/**
+ * MANEJO DE ERRORES
+ * https://expressjs.com/es/guide/error-handling.html
+ */
 
-app.use(cors());
-
-//ROUTES
-app.use('/producto', rtsProducto);
-app.use('/usuario', rtsUsuario);
-app.use('/cliente', rtsCliente);
-// app.use('/pedido', rtsPedido);
+/**
+ * ROUTES
+ */
+app.use("/", rtsRoot);
+app.use("/producto", rtsProducto);
+app.use("/usuario", rtsUsuario);
+app.use("/cliente", rtsCliente);
+app.use("/pedidos", rtsPedido);
 // app.use(routes);
 
-
-
-// SERVER 
-app.listen(4000, (err) => {
-    if(err) console.error('console.error: ' + err);
-    console.info('\x1b[1m\x1b[34m[------>] Servidor CORRIENDO =>\x1b[0m localhost:3000');
+/**
+ * SERVER
+ */
+app.listen(config.PORT, (err) => {
+  if (err) {
+    console.error("[------>] Servidor ERROR: \n\n" + err);
+  } else {
+    console.info(
+      `\x1b[1m\x1b[34m[------>] Servidor CORRIENDO =>\x1b[0m localhost:\x1b[1m\x1b[34m${config.PORT}`
+    );
+  }
 });
