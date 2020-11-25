72f736c7f89b7a754eb93d1843d6b4a29a7af4cf
Jimprd <joelimparado@gmail.com>
2020-11-25 18:55:47 -0300

    Se quitan estilos por defecto de react y se importa framework semantic UI. Se añade Ruteo.

--- front/tienda/src/App.js
+++ front/tienda/src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import "semantic-ui-react";
 
-function App() {
+
+/**
+ * IMPORTANDO MIS COMPONENTES
+ */
+/**
+ * TIENDA 
+ */
+import Home from "./components/pages/Home/Home";
+
+import Login from "./components/Login";
+
+// import Producto from "./components/pages/Productos/Producto";
+import ProductoDetalle from "./components/pages/Productos/ProductoDetalle";
+import Productos from "./components/pages/Productos/Productos";
+
+import Registrate from "./components/pages/Usuarios/Registrate";
+import PerfilUsuario from "./components/pages/Usuarios/ClientePerfil";
+
+/**
+ * DASHBOARD 
+ */
+import Dashboard from "./components/Dashboard/Dashboard";
+
+import DCategorias from "./components/Dashboard/Categorias/Categorias"
+import DCategoriaNueva from "./components/Dashboard/Categorias/Categoria_nueva"
+
+import DLogin from "./components/Dashboard/Login";
+
+import DProductos from "./components/Dashboard/Productos/Productos";
+import DProductoNuevo from "./components/Dashboard/Productos/Producto_nuevo";
+import DProducto from "./components/Dashboard/Productos/Producto";
+
+import DUsuarios from "./components/Dashboard/Usuarios/Usuarios";
+import DUsuarioNuevo from "./components/Dashboard/Usuarios/UsuarioNuevo";
+
+
+/**
+ * APP
+ */
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      {/* TIENDA */}
+      <Switch>
+        <Route path="/" component={Home} exact></Route>
+
+        <Route path="/login" component={Login} exact></Route>
+
+
+        <Route path="/producto/_id/:_id" component={ProductoDetalle} exact></Route>
+        <Route path="/productos/" component={Productos} exact></Route>
+
+        <Route path="/registrate/" component={Registrate} exact></Route>
+        <Route path="/micuenta/" component={PerfilUsuario} exact></Route>
+      </Switch>
+
+
+      {/* DASHBOARD */}
+      <Switch>
+        <Route path="/dashboard/" component={Dashboard} exact></Route>
+
+        <Route path="/dashboard/categorias" component={DCategorias} exact></Route>
+        <Route path="/dashboard/categoria-nueva" component={DCategoriaNueva} exact></Route>
+
+        <Route path="/dashboard/Login" component={DLogin} exact></Route>
+
+
+        <Route path="/dashboard/productos" component={DProductos} exact></Route>
+        <Route path="/dashboard/producto/_id/:_id" component={DProducto} exact></Route>
+        <Route
+          path="/dashboard/producto-nuevo" component={DProductoNuevo} exact
+        ></Route>
+
+        <Route path="/dashboard/usuarios" component={DUsuarios} exact></Route>
+
+        <Route path="/dashboard/usuario-nuevo" component={DUsuarioNuevo} exact></Route>
+      </Switch>
+      
+    </Router>
   );
 }
-
-export default App;
